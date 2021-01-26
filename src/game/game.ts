9e006da13ee525d923e9a29a6f3781d33a21d1b9
moritz <theincxption@gmail.com>
2021-01-26 15:08:12 +0100

    :art: Remove unnecessary await calls and async functions

--- src/game/game.ts
+++ src/game/game.ts
@@ -1,15 +1,13 @@
 import { renderCanvas } from "./render-core.js";
 
 export const canvas = document.getElementById("main-canvas") as HTMLCanvasElement
-export const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!!;
+export const ctx: CanvasRenderingContext2D = canvas.getContext("2d")!!
 
-(async () => {
-    window.addEventListener("resize", async () => await setCanvasDimensions())
-    await setCanvasDimensions()
-    await draw()
-})()
+window.addEventListener("resize", async () => await setCanvasDimensions())
+setCanvasDimensions()
+draw()
 
-async function setCanvasDimensions() {
+function setCanvasDimensions() {
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
 
@@ -17,10 +15,10 @@ async function setCanvasDimensions() {
     console.log("canvas.width =", canvas.width)
     console.log("canvas.height =", canvas.height)
 
-    await draw()
+    draw()
 }
 
-async function draw() {
+function draw() {
     ctx.fillStyle = "rgb(0, 0, 0)"
     ctx.fillRect(0, 0, canvas.width, canvas.height)
 
