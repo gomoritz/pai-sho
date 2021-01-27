d1812c17c012fe7fe96672e2703afa369b47c6e2
moritz <theincxption@gmail.com>
2021-01-27 14:51:25 +0100

    :children_crossing: Create "subtract" and "distanceBetween" utility functions for Points

--- src/game/shapes/point.ts
+++ src/game/shapes/point.ts
@@ -6,3 +6,13 @@ export default interface Point {
 export function add(a: Point, b: Point): Point {
     return { x: a.x + b.x, y: a.y + b.y }
 }
+
+export function subtract(a: Point, b: Point): Point {
+    return { x: a.x - b.x, y: a.y - b.y }
+}
+
+export function distanceBetween(a: Point, b: Point): number {
+    const distanceX = Math.abs(b.x - a.x)
+    const distanceY = Math.abs(b.y - a.y)
+    return Math.sqrt(Math.pow(distanceX, 2) + Math.pow(distanceY, 2))
+}
