db91077c024a6a13a0ffa8b1d77ad34e6c08dd18
moritz <theincxption@gmail.com>
2021-01-29 20:03:01 +0100

    :children_crossing: Add more utility and logic functions to field.ts

--- src/game/logic/field.ts
+++ src/game/logic/field.ts
@@ -45,6 +45,34 @@ export default class Field {
         return null
     }
 
+    isNeighbourField(other: Field): boolean {
+        const dx = this.x - other.x;
+        const dy = this.y - other.y;
+
+        if (Math.abs(dx) == Math.abs(dy) && Math.abs(dx) == 1) {
+            return true
+        }
+
+        return Math.abs(dx) + Math.abs(dy) == 1
+    }
+
+    getNeighbourFields(): Field[] {
+        return [
+            this.getRelativeField(Direction.TOP),
+            this.getRelativeField(Direction.TOP_RIGHT),
+            this.getRelativeField(Direction.RIGHT),
+            this.getRelativeField(Direction.BOTTOM_RIGHT),
+            this.getRelativeField(Direction.BOTTOM),
+            this.getRelativeField(Direction.BOTTOM_LEFT),
+            this.getRelativeField(Direction.LEFT),
+            this.getRelativeField(Direction.TOP_LEFT),
+        ].filter(it => it != null).map(it => it!!)
+    }
+
+    getFieldBetween(other: Field): Field | null {
+        return gameBoard.getField(this.x - (this.x - other.x) / 2, this.y - (this.y - other.y) / 2)
+    }
+
     highlight() {
         const { x, y } = this.translateToPoint()!!
         const center = gameBoardRenderer.center
