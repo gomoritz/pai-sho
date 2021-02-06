4ef0fc7cbe34b3d701be54c5710d376e87a06679
moritz <theincxption@gmail.com>
2021-02-06 19:17:37 +0100

    :sparkles: Advance debug overlay with more tile and field highlights

--- src/game/objects/debug-game-overview.ts
+++ src/game/objects/debug-game-overview.ts
@@ -1,6 +1,9 @@
 import RenderObject from "./render-object.js";
 import { ctx, isDebug } from "../game.js";
 import { GameStartPacket } from "../../shared/events/game-start.js";
+import { movingTile } from "../logic/tile-interaction.js";
+import { add } from "../../shared/utils/point.js";
+import { gameBoardRenderer } from "../render-core.js";
 
 export default class DebugGameOverview extends RenderObject {
     private static instance: DebugGameOverview
@@ -30,5 +33,23 @@ export default class DebugGameOverview extends RenderObject {
         lines.forEach((line, index) => {
             ctx.fillText(line, 10, 25 + (20 * index))
         })
+
+        if (movingTile != null) {
+            movingTile.getFieldsForPossibleMoves().forEach(field => {
+                const point = add(gameBoardRenderer.center, field.translateToPoint()!!)
+                ctx.fillStyle = "#e10ebf"
+                ctx.beginPath()
+                ctx.arc(point.x, point.y, 4, 0, Math.PI * 2)
+                ctx.fill()
+            })
+
+            movingTile.getTilesWhichCouldThrow().forEach(tile => {
+                const point = add(gameBoardRenderer.center, tile.field!!.translateToPoint()!!)
+                ctx.fillStyle = "#e1580e"
+                ctx.beginPath()
+                ctx.arc(point.x, point.y, 4, 0, Math.PI * 2)
+                ctx.fill()
+            })
+        }
     }
 }
