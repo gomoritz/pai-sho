4308732fcd3ef362308d8867e823161734184592
moritz <theincxption@gmail.com>
2021-02-07 14:33:09 +0100

    :sparkles: Add lobby object to every room in which players wait for the game to start

--- src/server/room/game-room.ts
+++ src/server/room/game-room.ts
@@ -2,6 +2,7 @@ import Player from "../objects/player.js";
 import PaiShoGame from "../game/pai-sho-game.js";
 import { TileMoveEvent, TileMovePacket } from "../../shared/events/tile-move.js";
 import { PassChainJumpEvent } from "../../shared/events/pass-chain-jump.js";
+import Lobby from "./lobby.js";
 
 export default class GameRoom {
     playerA: Player | null = null
@@ -9,11 +10,12 @@ export default class GameRoom {
     allPlayers: Player[] = []
 
     game: PaiShoGame = new PaiShoGame(this)
+    lobby: Lobby = new Lobby(this)
 
     constructor(public id: string) {
     }
 
-    addPlayerToRoom(player: Player) {
+    addPlayerToGame(player: Player) {
         if (this.playerA == null) {
             this.playerA = player
             this.game.currentPlayer = player
@@ -25,7 +27,7 @@ export default class GameRoom {
 
         this.allPlayers.push(player)
         player.socket.join(this.id)
-        player.socket.on("disconnect", () => this.removePlayerFromRoom(player))
+        player.socket.on("disconnect", () => this.removePlayerFromGame(player))
         player.socket.on(TileMoveEvent, (packet: TileMovePacket) => this.game.handleTileMove(player, packet))
         player.socket.on(PassChainJumpEvent, () => this.game.handlePassChainJump(player))
 
@@ -36,7 +38,7 @@ export default class GameRoom {
         }
     }
 
-    removePlayerFromRoom(player: Player) {
+    removePlayerFromGame(player: Player) {
         if (this.playerA == player) {
             this.playerA = null
         } else if (this.playerB == player) {
@@ -57,9 +59,5 @@ export default class GameRoom {
 
     isFull(): boolean {
         return this.playerA != null && this.playerB != null
-    }
-
-    isUsernameTaken(username: string): boolean {
-        return this.allPlayers.some(it => it.username == username)
     }
 }
---
+++ src/server/room/lobby.ts
@@ -0,0 +1,88 @@
+import GameRoom from "./game-room.js";
+import { Socket } from "socket.io";
+import { ChangeNameEvent, ChangeNamePacket } from "../../shared/events/change-name.js";
+import { PublishLobbyEntitiesEvent, PublishLobbyEntitiesPacket } from "../../shared/events/publish-lobby-entities.js";
+import { RedirectToGameEvent, RedirectToGamePacket, StartRoomEvent } from "../../shared/events/start-room.js";
+import { serializeGameKey } from "../utils/tokenizer.js";
+import { RoomManager } from "./room-manager.js";
+
+export default class Lobby {
+    entities: LobbyEntity[] = []
+
+    constructor(private room: GameRoom) {
+    }
+
+    addToLobby(socket: Socket, username: string): boolean {
+        if (this.entities.length >= 2) return false
+        if (!this.validateName(username)) return false
+
+        const entity = new LobbyEntity(socket, username)
+        this.entities.push(entity)
+
+        socket.on(ChangeNameEvent, (packet: ChangeNamePacket) => this.renameEntity(entity, packet))
+        socket.on(StartRoomEvent, () => this.startRoom(entity))
+        socket.on("disconnect", () => this.removeFromLobby(entity))
+        this.publishLobbyEntities()
+        console.log(`${username} joined lobby of room ${this.room.id}`)
+        return true
+    }
+
+    removeFromLobby(entity: LobbyEntity) {
+        this.entities.splice(this.entities.indexOf(entity), 1)
+        this.publishLobbyEntities()
+        console.log(`${entity.username} left lobby of room ${this.room.id}`)
+
+        if (this.entities.length == 0) {
+            RoomManager.deleteRoom(this.room)
+        }
+    }
+
+    publishLobbyEntities() {
+        this.entities.forEach(entity => {
+            const packet: PublishLobbyEntitiesPacket = {
+                entities: this.entities.map(it => ({
+                    username: it.username,
+                    me: it == entity
+                }))
+            }
+            entity.socket.emit(PublishLobbyEntitiesEvent, packet)
+        })
+    }
+
+    renameEntity(entity: LobbyEntity, packet: ChangeNamePacket) {
+        if (!this.validateName(packet.newUsername)) return
+        console.log(`Renamed ${entity.username} to ${packet.newUsername}`)
+        entity.username = packet.newUsername
+        this.publishLobbyEntities()
+    }
+
+    startRoom(entity: LobbyEntity) {
+        if (this.entities.length != 2) return
+
+        this.entities.forEach(entity => {
+            const gameKey = serializeGameKey({ roomId: this.room.id, username: entity.username })
+            const packet: RedirectToGamePacket = { gameKey }
+            entity.socket.emit(RedirectToGameEvent, packet)
+        })
+
+        console.log(`${entity.username} started the room ${this.room.id}`)
+    }
+
+    validateName(name: string): boolean {
+        if (name.length < 3) return false
+        if (name.length > 16) return false
+
+        const allowedCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_. äöüÄÖÜ"
+        for (let i = 0; i < name.length; i++) {
+            const char = name.charAt(i)
+            if (allowedCharacters.indexOf(char) == -1)
+                return false
+        }
+        return true
+    }
+}
+
+export class LobbyEntity {
+    constructor(public socket: Socket, public username: string) {
+    }
+}
