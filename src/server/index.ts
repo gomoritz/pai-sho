fe8fe5232c9884d89f3628f7bd3920667a3f5e7e
moritz <theincxption@gmail.com>
2021-02-07 14:32:05 +0100

    :sparkles: Create /room and /create_room route

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,7 @@
 import express from "express"
 import { createServer } from "http"
 import { attachSocketServer } from "./socket.js";
+import { generateUUID, RoomManager } from "./room/room-manager.js";
 
 const app = express()
 const http = createServer(app)
@@ -8,8 +9,12 @@ const port = 1616
 
 app.use("/game", express.static("src/client/game", { index: "game.html" }))
 app.use("/play", express.static("src/client/play", { index: "play.html" }))
+app.use("/room", express.static("src/client/room", { index: "room.html" }))
 app.use("/resources", express.static("resources"))
 app.use("/shared", express.static("src/shared"))
+
+app.get("/create_room", (req, res) => res.redirect(`/room?id=${RoomManager.createRoom(generateUUID()).id}`))
+app.get("/", (req, res) => res.redirect("/play"))
 
 attachSocketServer(http)
 
