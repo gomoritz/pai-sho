a9bcf16675f901221913a67bd5b2c6f954f0a03d
moritz <theincxption@gmail.com>
2021-02-07 11:46:07 +0100

    :sparkles: Create /play route with ui

--- src/server/index.ts
+++ src/server/index.ts
@@ -7,6 +7,7 @@ const http = createServer(app)
 const port = 1616
 
 app.use("/game", express.static("src/client/game", { index: "game.html" }))
+app.use("/play", express.static("src/client/play", { index: "play.html" }))
 app.use("/resources", express.static("resources"))
 app.use("/shared", express.static("src/shared"))
 
